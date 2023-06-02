8c209b53ecd614f3418383cf12b220015cf32e3d
CamiloEscobaR95 <escobarcamilo110@gmail.com>
2023-06-02 11:24:33 -0500

    se carga la carpeta main

--- src/test/java/co/com/allianz/stepdefinitions/Hooks.java
+++ src/test/java/co/com/allianz/stepdefinitions/Hooks.java
@@ -5,6 +5,7 @@ public class Hooks {
     public void setStage() {
         //aca pdodemos llamar el metodo de ir la homepage y hacer login correcto para
         // todos los casos que se correran
+        //y la creación del actor
     }
 
 }
